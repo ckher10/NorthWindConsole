5fe4e869309bad67dda8ab8e8633eb34918ac7c2
Cheng Her <ckher96@gmail.com>
2025-04-17 13:18:42 -0500

    [1.09] Create user options

--- Program.cs
+++ Program.cs
@@ -1,11 +1,35 @@
 ﻿using NLog;
- string path = Directory.GetCurrentDirectory() + "//nlog.config";
- 
- // create instance of Logger
- var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
- 
- logger.Info("Program started");
- 
- Console.WriteLine("Hello World!");
- 
- logger.Info("Program ended");
+using System.Linq;
+using NorthwindConsole.Model;
+string path = Directory.GetCurrentDirectory() + "//nlog.config";
+
+// create instance of Logger
+var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
+
+logger.Info("Program started");
+
+do
+{
+  Console.WriteLine("1) Display categories");
+  Console.WriteLine("2) Add category");
+  Console.WriteLine("Enter to quit");
+  string? choice = Console.ReadLine();
+  Console.Clear();
+  logger.Info("Option {choice} selected", choice);
+
+  if (choice == "1")
+  {
+    // display categories
+  }
+  else if (choice == "2")
+  {
+    // Add category
+  }
+  else if (String.IsNullOrEmpty(choice))
+  {
+    break;
+  }
+  Console.WriteLine();
+} while (true);
+
+logger.Info("Program ended");
