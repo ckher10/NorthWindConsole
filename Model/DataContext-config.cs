059bbcdd38248b795a3e92672c0a7f5884cfc8be
Cheng Her <ckher96@gmail.com>
2025-04-17 13:29:16 -0500

    [1.10] Display categories

--- Model/DataContext-config.cs
+++ Model/DataContext-config.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
-namespace NorthwindConsole.Model;
+namespace NorthWindConsole.Model;
 
 public partial class DataContext : DbContext
 {
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using NLog;
 using System.Linq;
-using NorthwindConsole.Model;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using NorthWindConsole.Model;
 string path = Directory.GetCurrentDirectory() + "//nlog.config";
 
 // create instance of Logger
@@ -20,6 +22,22 @@ do
   if (choice == "1")
   {
     // display categories
+    var configuration = new ConfigurationBuilder()
+            .AddJsonFile($"appsettings.json");
+
+    var config = configuration.Build();
+
+    var db = new DataContext();
+    var query = db.Categories.OrderBy(p => p.CategoryName);
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"{query.Count()} records returned");
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    foreach (var item in query)
+    {
+      Console.WriteLine($"{item.CategoryName} - {item.Description}");
+    }
+    Console.ForegroundColor = ConsoleColor.White;
   }
   else if (choice == "2")
   {
